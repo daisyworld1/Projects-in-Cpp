2f5bd90eed6cec81f2258f7e95bdea732c31b85b
Youzhu Shi <39037277+daisyworld2018@users.noreply.github.com>
2019-03-02 01:03:41 -0800

    Rename Store.hpp to OnlineStoreSimulator/Store.hpp

---
+++ OnlineStoreSimulator/Store.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef STORE_HPP
+#define STORE_HPP
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+#include "Customer.hpp"
+
+class Store
+{
+private:
+	std::vector<Product*> inventory;
+	std::vector<Customer*> members;
+public:
+	void addProduct(Product* p);
+	void addMember(Customer* c);
+	Product* getProductFromID(std::string);
+	Customer* getMemberFromID(std::string);
+	void productSearch(std::string str);
+	void addProductToMemberCart(std::string pID, std::string mID);
+	void checkOutMember(std::string mID);
+};
+
+#endif
